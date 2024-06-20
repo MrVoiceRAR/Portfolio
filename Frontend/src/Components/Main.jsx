c78f750c5ab419c6a5b45dc77bfeee6087f51446
MrVoiceRAR <lealoria117@gmail.com>
2024-06-20 17:51:56 -0400

    Tools v0.1

--- Frontend/src/Components/Main.jsx
+++ Frontend/src/Components/Main.jsx
@@ -29,14 +29,56 @@ const HomePage = () => {
         </div>
       </div>
 
-      <section className="projects">
-        <h1>My Projects</h1>
-        <div className="project-list">
-          <div className="project-item">Project 1</div>
-          <div className="project-item">Project 2</div>
-          <div className="project-item">Project 3</div>
+      <div className="projects">
+        <h1 data-text="My Skills">My Skills</h1>
+        <div className="skill-list">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+            explicabo alias nulla culpa laudantium, aliquid aut debitis fugit
+            animi exercitationem minus esse recusandae earum iste quos suscipit
+            rem quisquam repudiandae!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+            mollitia officiis eveniet neque reiciendis minima dicta voluptatibus
+            eum ducimus. Illum incidunt, voluptatem reiciendis unde deleniti
+            saepe minus et repellendus ullam!
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis,
+            expedita? Eum sapiente illum omnis nulla tempore neque id animi
+            iusto, ipsam repellat atque minima. Harum aperiam molestiae
+            exercitationem omnis vitae!
+          </p>
         </div>
-      </section>
+        <h2>Tools</h2>
+        <div className="Tools">
+          <div className="tool">
+            <img src="" alt="HTML" />
+            <p>HTML</p>
+          </div>
+          <div className="tool">
+            <img src="" alt="SCSS" />
+            <p>SCSS</p>
+          </div>
+          <div className="tool">
+            <img src="" alt="JS" />
+            <p>Js</p>
+          </div>
+          <div className="tool">
+            <img src="" alt="ReactJS" />
+            <p>ReactJS</p>
+          </div>
+          <div className="tool">
+            <img src="" alt="Git" />
+            <p>Git</p>
+          </div>
+          <div className="tool">
+            <img src="" alt="MySql" />
+            <p>MySQL</p>
+          </div>
+        </div>
+      </div>
 
       <section className="contact">
         <h1>Contact Me</h1>
