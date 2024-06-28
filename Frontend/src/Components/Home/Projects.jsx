52c8905c275212638e1ce89c77a31f9b09cf5d44
MrVoiceRAR <lealoria117@gmail.com>
2024-06-28 12:23:44 -0400

    Project init

--- Frontend/src/Components/Home/Projects.jsx
+++ Frontend/src/Components/Home/Projects.jsx
@@ -5,9 +5,35 @@ const Projects = () => {
       <div className="ProjectLabel">
         <h1>Projects</h1>
       </div>
-      <div className="project1"></div>
-      <div className="project2"></div>
-      <div className="project3"></div>
+      <div className="project1">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum aut
+          quibusdam ipsa possimus, aspernatur expedita assumenda, quidem
+          voluptatibus minima in officiis eveniet consequatur fugit cupiditate
+          earum animi numquam incidunt praesentium!
+        </p>
+        <img src="#" alt="project1" />
+      </div>
+      <div className="project2">
+        {" "}
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum aut
+          quibusdam ipsa possimus, aspernatur expedita assumenda, quidem
+          voluptatibus minima in officiis eveniet consequatur fugit cupiditate
+          earum animi numquam incidunt praesentium!
+        </p>
+        <img src="#" alt="project1" />
+      </div>
+      <div className="project3">
+        {" "}
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum aut
+          quibusdam ipsa possimus, aspernatur expedita assumenda, quidem
+          voluptatibus minima in officiis eveniet consequatur fugit cupiditate
+          earum animi numquam incidunt praesentium!
+        </p>
+        <img src="#" alt="project1" />
+      </div>
     </div>
   );
 };
